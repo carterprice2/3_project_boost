3049b6dd475ce4f7892bbc5ece4864154e47a54c
lprice8 <carterprice2@gmail.com>
2018-12-18 11:47:19 -0500

    added audio and Rocket thrust&rotation

---
+++ Assets/Rocket.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource audiosource;
+
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audiosource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))    //can thrust while rotating
+        {
+            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audiosource.isPlaying)
+            {
+                audiosource.Play();
+            }
+            
+        }else
+        {
+            audiosource.Stop();
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Left");
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
